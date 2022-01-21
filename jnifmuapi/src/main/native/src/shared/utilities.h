50df7dc20fcc1378f80edfb0d05f40c3ce2eae8b
Kenneth Lausdahl <Kenneth.GuldbrandtLausdahl@agcocorp.com>
2022-01-21 13:45:32 +0100

    added cpp version of declare array

--- jnifmuapi/src/main/native/src/shared/utilities.h
+++ jnifmuapi/src/main/native/src/shared/utilities.h
@@ -5,10 +5,20 @@
 #ifndef FMUAPI_UTILITIES_H
 #define FMUAPI_UTILITIES_H
 
+#ifdef __cplusplus
+#define DECLARE_ARRAY(type, name, length)             \
+  auto *name = (type *)malloc(sizeof(type) * length); \
+  if (name == NULL) {                                 \
+    throwException(env, "malloc r failed");           \
+  }
+#else
 #define DECLARE_ARRAY(type, name, length)             \
   type *name = (type *)malloc(sizeof(type) * length); \
   if (name == NULL) {                                 \
     throwException(env, "malloc r failed");           \
   }
+#endif
+
+
 
 #endif //FMUAPI_UTILITIES_H
